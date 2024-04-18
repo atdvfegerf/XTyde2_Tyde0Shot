a89f8ca3446208d82303483898e04863daea9df5
atdvfegerf <143095093+atdvfegerf@users.noreply.github.com>
2024-04-18 16:32:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,21 +23,19 @@ def clone(text, url, language):
     # Extract the zip file to the current directory
     with zipfile.ZipFile("temp.zip", "r") as zip_ref:
         zip_ref.extractall()
-    # Get the audio file paths
-    audio_files = [f for f in os.listdir(".") if f.endswith(".wav")]
-    # Use each audio file for voice cloning
-    for audio_file in audio_files:
-        tts.tts_to_file(text=text, speaker_wav=audio_file, language=language, file_path=f"./output_{audio_file}.wav")
+    # Get the audio file path
+    audio_file = [f for f in os.listdir(".") if f.endswith(".wav")][0]
+    # Use the audio file for voice cloning
+    tts.tts_to_file(text=text, speaker_wav=audio_file, language=language, file_path="./output.wav")
     # Delete the temporary files
-    for audio_file in audio_files:
-        os.remove(audio_file)
+    os.remove(audio_file)
     os.remove("temp.zip")
-    # Return the output file paths
-    return [f"./output_{audio_file}.wav" for audio_file in audio_files]
+    # Return the output file path
+    return "./output.wav"
 
 iface = gr.Interface(fn=clone,
                      inputs=["text", gr.components.Text(label="URL"), gr.Dropdown(choices=["en", "es", "fr", "de", "it", "ja", "zh-CN", "zh-TW"], label="Language")],
-                     outputs=gr.Audio(type='filepath', multi=True),
+                     outputs=gr.Audio(type='filepath'),
                      title='Voice Clone',
                      description="""
                      by [Angetyde](https://youtube.com/@Angetyde?si=7nusP31nTumIkPTF) and [Tony Assi](https://www.tonyassi.com/ )
@@ -46,4 +44,4 @@ iface = gr.Interface(fn=clone,
                      """,
                      theme=gr.themes.Base(primary_hue="teal", secondary_hue="teal", neutral_hue="slate"))
 
-iface.launch(share=True)
+iface.launch()
