93a7281fa7118460f66f4ef763dc0eb2d5811d28
atdvfegerf <143095093+atdvfegerf@users.noreply.github.com>
2024-05-05 23:17:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,74 +1,87 @@
 import gradio as gr
-import torch
 import os
-import zipfile
 import requests
+import torch
+import zipfile
 from TTS.api import TTS
 from pydub import AudioSegment
 
-# Set environment variable
 os.environ["COQUI_TOS_AGREED"] = "1"
 
-# Define constants
 MODEL_PATH = "tts_models/multilingual/multi-dataset/xtts_v2"
 LANGUAGES = ["en", "es", "fr", "de", "it", "pt", "pl", "tr", "ru", "nl", "cs", "ar", "zh-cn", "ja", "hu", "ko", "hi"]
 AUDIO_FORMATS = [".wav", ".mp3", ".flac", ".mp4"]
 
-# Automatically detect and use GPU if available
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
-# Load TTS model
 tts = TTS(MODEL_PATH).to(device)
 
-def convert_to_wav(audio_file):
-    # Extract file extension
-    file_extension = os.path.splitext(audio_file)[-1].lower()
+def download_audio_file(url):
+    try:
+        response = requests.get(url)
+        file_extension = os.path.splitext(url)[-1].lower()
+        file_name = f"temp{file_extension}"
+        with open(file_name, "wb") as f:
+            f.write(response.content)
+        return file_name
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading audio file: {e}")
+        return None
 
-    # Convert audio file to .wav format
-    if file_extension != ".wav":
-        audio = AudioSegment.from_file(audio_file)
+def extract_zip_file(zip_file):
+    try:
+        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+            zip_ref.extractall()
+        return True
+    except zipfile.BadZipfile as e:
+        print(f"Error extracting zip file: {e}")
+        return False
+
+def convert_to_wav(input_audio_file):
+    file_extension = os.path.splitext(input_audio_file)[-1].lower()
+    if file_extension!= ".wav":
+        audio = AudioSegment.from_file(input_audio_file)
         audio.export("temp.wav", format="wav")
-        os.remove(audio_file)
-        audio_file = "temp.wav"
+        os.remove(input_audio_file)
+        return "temp.wav"
+    return input_audio_file
 
-    return audio_file
-
-def clone(text, url, language):
-    # Download zip file
-    response = requests.get(url)
-    with open("temp.zip", "wb") as f:
-        f.write(response.content)
-
-    # Extract audio file from zip archive
-    with zipfile.ZipFile("temp.zip", "r") as zip_ref:
-        for file in zip_ref.namelist():
-            if os.path.splitext(file)[-1].lower() in AUDIO_FORMATS:
-                zip_ref.extract(file, ".")
-                audio_file = file
-                break
-
-    # Convert audio file to .wav format
-    audio_file = convert_to_wav(audio_file)
-
-    # Generate audio using TTS model
-    tts.tts_to_file(text=text, speaker_wav=audio_file, language=language, file_path="./output.wav")
-
-    # Clean up
-    os.remove(audio_file)
-    os.remove("temp.zip")
-
+def synthesize_text(text, input_audio_file, language):
+    input_audio_file = convert_to_wav(input_audio_file)
+    tts.tts_to_file(text=text, speaker_wav=input_audio_file, language=language, file_path="./output.wav")
     return "./output.wav"
 
-# Create Gradio interface
+def clone(text, input_file, language, url=None, use_url=False):
+    if use_url:
+        if url is None:
+            return None
+        input_audio_file = download_audio_file(url)
+        if input_audio_file is None:
+            return None
+    else:
+        if input_file is None:
+            return None
+        if input_file.name.endswith(".zip"):
+            if extract_zip_file(input_file):
+                input_audio_file = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith(tuple(AUDIO_FORMATS))]
+                if len(input_audio_file) == 1:
+                    input_audio_file = input_audio_file[0]
+                else:
+                    return "Error: Please select a single audio file from the extracted files."
+        else:
+            input_audio_file = input_file.name
+
+    output_file_path = synthesize_text(text, input_audio_file, language)
+    return output_file_path
+
 iface = gr.Interface(
     fn=clone,
-    inputs=["text", gr.components.Text(label="URL"), gr.Dropdown(choices=LANGUAGES, label="Language")],
+    inputs=["text", gr.File(label="Input File", file_types=[".zip", *AUDIO_FORMATS]), gr.Dropdown(choices=LANGUAGES, label="Language"), gr.Text(label="URL"), gr.Checkbox(label="Use URL", value=False)],
     outputs=gr.Audio(type='filepath'),
     title='Voice Clone',
     description=""" by [Angetyde](https://youtube.com/@Angetyde?si=7nusP31nTumIkPTF) and [Tony Assi](https://www.tonyassi.com/ ) use this colab with caution <3. """,
     theme=gr.themes.Base(primary_hue="teal", secondary_hue="teal", neutral_hue="slate")
 )
 
-# Launch Gradio interface
 iface.launch(share=True)
